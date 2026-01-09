9b4d3cec78846baab31f110824c8f775bf794dea
Seamus <seamus.svahn@tuni.fi>
2026-01-09 15:14:44 +0200

    Fix merge conflicts

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,2 @@
 print("Hello world!")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello once again")
-=======
 print("Hello darkness my old friend")
->>>>>>> mergeconflict/master
-=======
-print("I've come to talk with you again")
->>>>>>> mergeconflict/feature/modify-print
